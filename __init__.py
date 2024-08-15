84e44f36d0b5b3925c311a417f5b2fac64cad3c7
Jim Studt <jim@studt.net>
2024-08-15 14:04:43 -0500

    Add the 'zoom on segment' node. Fixed.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,8 @@
-from .nodes.crop_chest_node import *
 from .nodes.zoom_focus import *
 from .nodes.text_to_string_list import *
 from .nodes.dictionaries import *
 
 NODE_CLASS_MAPPINGS = {
-    "CropChest": CropChestNode,
-
     "ZoomFocus" : ZoomFocus,
     "TextToStringList": TextToStringList,
     "ChooseFromStringList": ChooseFromStringList,
@@ -16,8 +13,6 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "CropChest": "Crop to Chest, necklace, not boobs",
-
     "ZoomFocus": "Zoom On Segment",
     
     "TextToStringList": "Text To String List",
